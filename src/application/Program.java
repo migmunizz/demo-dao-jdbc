9e146352789273782e7d412485fcfed4a5f83b81
Miguel Muniz <mr.muniz4230@gmail.com>
2026-05-23 20:52:05 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import Model.entites.Department;
+import Model.entites.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args){
         Department obj = new Department(1,"Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(12,"bob@gmail.com","Bob", LocalDate.now(),2000.0,obj);
+        System.out.println(seller);
     }
 }
